8a159efbcb02bab1e3cbdc801ac5716887f4c5fc
Kalle <kalle@makewebo.com>
2015-02-24 09:30:47 +0100

    Testar commit
    ny commit yay

--- footer.php
+++ footer.php
@@ -23,7 +23,7 @@
 *  @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
 *  International Registered Trademark & Property of PrestaShop SA
 */
-
+// Testar commit
 if (isset(Context::getContext()->controller))
 	$controller = Context::getContext()->controller;
 else
